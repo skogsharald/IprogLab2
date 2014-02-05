6e4f349575b56352bd8726f8a23581d0fa385d20
Ludvig Jansson <ludjan@kth.se>
2014-02-05 16:22:40 +0100

    Created menu and welcome views.

--- src/se/kth/csc/iprog/dinnerplanner/android/MainActivity.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/MainActivity.java
@@ -14,10 +14,10 @@ public class MainActivity extends Activity {
     	
     	// Set the view for the main activity screen
     	// it must come before any call to findViewById method
-        setContentView(R.layout.menu);
+        setContentView(R.layout.activity_main);
         
     	// Creating the view class instance
-    	ExampleView mainView = new ExampleView(findViewById(R.id.this_is_example_view_id));
+    	//ExampleView mainView = new ExampleView(findViewById(R.id.this_is_example_view_id));
 
     }
 
