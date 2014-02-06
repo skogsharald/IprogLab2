ec3312abbef4cef28d3d848fd1c03267b7814367
Annica Alstermark <annica.alstermark@gmail.com>
2014-02-06 13:55:33 +0100

    Added course view controller and deleted the example files

---
+++ src/se/kth/csc/iprog/dinnerplanner/android/view/CourseView.java
@@ -0,0 +1,14 @@
+package se.kth.csc.iprog.dinnerplanner.android.view;
+
+import android.view.View;
+
+public class CourseView {
+	View view;
+	
+	public CourseView(View view){
+		// store in the class the reference to the Android View
+		this.view = view;
+
+		// Setup the rest of the view layout
+	}
+}
