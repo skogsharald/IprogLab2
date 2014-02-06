8c4ceb9809874181e1c8d93c0bb04f4aa2ea50f7
Annica Alstermark <annica.alstermark@gmail.com>
2014-02-06 16:15:21 +0100

    Added the model to some of the controllers. Very MVC.

--- src/se/kth/csc/iprog/dinnerplanner/android/view/MenuHeaderView.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/view/MenuHeaderView.java
@@ -1,14 +1,17 @@
 package se.kth.csc.iprog.dinnerplanner.android.view;
 
+import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
 import android.view.View;
 
 public class MenuHeaderView {
 	View view;
+	DinnerModel model;
 
-	public MenuHeaderView(View view) {
+	public MenuHeaderView(View view, DinnerModel model) {
 
 		// store in the class the reference to the Android View
 		this.view = view;
+		this.model= model;
 
 		// Setup the rest of the view layout
 	}
